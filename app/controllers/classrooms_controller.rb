ba5c2348938042d9e0dd32378565aac511030ad7
adam n <adam.nawroc@gmail.com>
2018-04-17 13:37:24 +0000

    automatically backed up by learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -2,7 +2,9 @@ class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
     @student = @classroom.oldest_student
-    
+
+    binding.pry
+
   end
 
   def index
